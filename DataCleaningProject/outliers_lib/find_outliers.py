5897f2be18054d269d458b5e4c2f73d12b909043
feelingcxld <sasedov07@gmail.com>
2023-11-22 14:44:24 +0300

    find_outliers_quantile function added

--- DataCleaningProject/outliers_lib/find_outliers.py
+++ DataCleaningProject/outliers_lib/find_outliers.py
@@ -39,8 +39,8 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     Args:
         data (pandas.DataFrame): набор данных
         feature (str): имя признака, на основе которого происходит поиск выбросов
-        left (float, optional): количество стандартных отклонений в левую сторону распределения. По умолчанию 1.5.
-        right (float, optional): количество стандартных в правую сторону распределения. По умолчанию 1.5.
+        left (float, optional): количество стандартных отклонений в левую сторону распределения. По умолчанию 3.
+        right (float, optional): количество стандартных в правую сторону распределения. По умолчанию 3.
         log_scale (bool, optional): режим логарифмирования. По умолчанию False - логарифмирование не применяется.
 
     Returns:
@@ -59,3 +59,25 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
 
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    """
+    Находит выбросы в данных используя квантили в качестве границ
+    Классический метод модифицирован путем добавления:
+    * ручного управления размером квантилей в обе стороны распределения
+
+    Args:
+        data (pandas.DataFrame): Набор данных
+        feature (str): Имя признака, на основе которого происходит поиск выбросов
+        left (float, optional): Левая граница. По умолчанию 0.01.
+        right (float, optional): Правая граница. По умолчанию 0.99.
+
+    Returns:
+        pandas.DataFrame: наблюдения, попавшие в разряд выбросов
+        pandas.DataFrame: очищенные данные, из которых исключены выбросы
+    """
+    x = data[feature]
+    lower_bound = x.quantile(left)
+    upper_bound = x.quantile(right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
